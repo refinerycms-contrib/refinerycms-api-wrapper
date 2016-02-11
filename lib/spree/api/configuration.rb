ffbe42d712cdfc1204bb0c40cac7a64d5c42244c
Ian Rodriguez <ian.rgz@gmail.com>
2016-02-11 13:47:29 -0700

    Add products client endpoints and test

--- lib/spree/api/configuration.rb
+++ lib/spree/api/configuration.rb
@@ -1,3 +1,5 @@
+require "faraday"
+
 module Spree
   module API
     class Configuration
@@ -6,6 +8,13 @@ module Spree
       def initialize
         @api_token = ""
         @api_url = ""
+      end
+
+      def connection
+        @_connection ||= ::Faraday.new(url: api_url, headers: { "X-Spree-Token" => api_token }) do |faraday|
+          faraday.request :url_encoded
+          faraday.adapter Faraday.default_adapter
+        end
       end
     end
   end
--- spec/spree/api_spec.rb
+++ spec/spree/api_spec.rb
@@ -9,5 +9,15 @@ module Spree
     it "proxies configuration to the block" do
       expect{ |probe| API.configure(&probe) }.to yield_control
     end
+
+    it "sets configuration options through the block" do
+      Spree::API.configure do |config|
+        config.api_token = "1234"
+        config.api_url = "api.test.com"
+      end
+
+      expect(Spree::API.configuration.api_token).to eq("1234")
+      expect(Spree::API.configuration.api_url).to eq("api.test.com")
+    end
   end
 end

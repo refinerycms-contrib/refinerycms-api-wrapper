44a8ecf562c14703b6ea33ba2e4c142e093cb4a7
Ian Rodriguez <ian.rgz@gmail.com>
2016-02-11 13:56:49 -0700

    Add variants client endpoints and tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,13 @@ require 'spree/api'
 require 'vcr'
 require 'pry'
 
+require "support/request_helpers"
+
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/support/vcr_cassettes'
   c.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  config.include Requests::JsonHelper
 end

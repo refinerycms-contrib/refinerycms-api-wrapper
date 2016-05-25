78d7175f7bf783740bebc53a232d72362f3a1a80
mhtremblay <mhtremblay@bisscomm.com>
2016-05-25 16:06:36 -0400

    remove useless files from spree

--- lib/refinery/api.rb
+++ lib/refinery/api.rb
@@ -1,12 +1,6 @@
 require "refinery/api/version"
 require "refinery/api/configuration"
 require "refinery/api/pages"
-require "refinery/api/products"
-require "refinery/api/variants"
-require "refinery/api/taxonomies"
-require "refinery/api/taxons"
-require "refinery/api/option_types"
-require "refinery/api/option_values"
 
 require "faraday"
 

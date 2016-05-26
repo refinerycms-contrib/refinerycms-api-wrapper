4d8c20a70e55257a33a55fd8caa2fbf8ca113055
Brice Sanchez <brice@bisscomm.com>
2016-05-26 15:30:27 -0400

    Improve dev tools

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in refinery-api.gemspec
 gemspec
+
+group :development, :test do
+  gem 'dotenv'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,22 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'refinery/api'
 require 'vcr'
 require 'pry'
+require 'dotenv'
+Dotenv.load
 
 require "support/request_helpers"
+
+ENV['REFINERYCMS_API_TOKEN'] ||= '123'
+ENV['REFINERYCMS_API_URL'] ||= 'http://localhost:3000'
+ENV['REFINERYCMS_API_PATH'] ||= '/api/v1'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/support/vcr_cassettes'
   c.hook_into :webmock
+
+  c.filter_sensitive_data("REFINERYCMS_API_TOKEN") { ENV["REFINERYCMS_API_TOKEN"] }
+  c.filter_sensitive_data("REFINERYCMS_API_URL") { ENV["REFINERYCMS_API_URL"] }
+  c.filter_sensitive_data("REFINERYCMS_API_PATH") { ENV["REFINERYCMS_API_PATH"] }
 end
 
 RSpec.configure do |config|
@@ -18,9 +28,9 @@ RSpec.configure do |config|
 
   config.before(:each) do
     Refinery::API.configure do |conf|
-      conf.api_token = "123"
-      conf.api_url = "http://localhost:3000"
-      conf.api_path = "/api/v1"
+      conf.api_token = ENV["REFINERYCMS_API_TOKEN"]
+      conf.api_url = ENV["REFINERYCMS_API_URL"]
+      conf.api_path = ENV["REFINERYCMS_API_PATH"]
     end
   end
 end
